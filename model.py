73413f3ca305c62c5ddd9b1d6ea2f4992561b889
Bikash <bikash11roy@gmail.com>
2018-03-25 02:33:09 +0600

    discriminator defined

--- model.py
+++ model.py
@@ -60,3 +60,38 @@ def generator_model():
     model = Model(inputs=inputs, outputs=outputs, name='Generator')
     model.summary()
     return model
+
+
+def discriminator_model():
+    """Build discriminator architecture."""
+    n_layers, use_sigmoid = 3, False
+    inputs = Input(shape=input_shape_discriminator)
+
+    x = Conv2D(filters=ndf, kernel_size=(4, 4), strides=2, padding='same')(inputs)
+    x = LeakyReLU(0.2)(x)
+
+    nf_mult, nf_mult_prev = 1, 1
+    for n in range(n_layers):
+        nf_mult_prev, nf_mult = nf_mult, min(2 ** n, 8)
+        x = Conv2D(filters=ndf * nf_mult, kernel_size=(4, 4), strides=2, padding='same')(x)
+        x = BatchNormalization()(x)
+        x = LeakyReLU(0.2)(x)
+
+    nf_mult_prev, nf_mult = nf_mult, min(2 ** n_layers, 8)
+    x = Conv2D(filters=ndf * nf_mult, kernel_size=(4, 4), strides=1, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(0.2)(x)
+
+    x = Conv2D(filters=1, kernel_size=(4, 4), strides=1, padding='same')(x)
+    if use_sigmoid:
+        x = Activation('sigmoid')(x)
+
+    x = Flatten()(x)
+    x = Dense(1024, activation='tanh')(x)
+    x = Dense(1, activation='sigmoid')(x)
+
+    model = Model(inputs=inputs, outputs=x, name='Discriminator')
+    model.summary()
+    return model
+
+
